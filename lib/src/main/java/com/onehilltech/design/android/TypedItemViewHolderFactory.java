cda1d67e1dabc580bd03db4c69ffadda34a3f3b5
James H. Hill <hilljh82@gmail.com>
2016-03-04 23:35:01 -0500

    Implemented typed view holder factory class

---
+++ lib/src/main/java/com/onehilltech/design/android/TypedItemViewHolderFactory.java
@@ -0,0 +1,32 @@
+package com.onehilltech.design.android;
+
+import android.view.View;
+
+import java.lang.reflect.Constructor;
+
+public class TypedItemViewHolderFactory <T extends ItemAdapter.ViewHolder>
+    extends SimpleItemViewHolderFactory
+{
+  private final Class <T> clazz_;
+
+  public TypedItemViewHolderFactory (int layoutId, Class <T> clazz)
+  {
+    super (layoutId);
+
+    this.clazz_ = clazz;
+  }
+
+  @Override
+  protected T onCreateViewHolder (View view)
+  {
+    try
+    {
+      Constructor <T> ctor = this.clazz_.getConstructor (View.class);
+      return ctor.newInstance (view);
+    }
+    catch (Exception e)
+    {
+      throw new RuntimeException ("Failed to create view holder", e);
+    }
+  }
+}
