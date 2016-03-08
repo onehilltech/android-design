0ffd071a9903303fe6dffb1a576fbec7e9933fde
James H. Hill <hilljh82@gmail.com>
2016-03-08 17:16:35 -0500

    Added DialogUtils for setting spacing for custom view

---
+++ lib/src/main/java/com/onehilltech/design/android/DialogUtils.java
@@ -0,0 +1,19 @@
+package com.onehilltech.design.android;
+
+import android.content.res.Resources;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+
+public class DialogUtils
+{
+  public void setView (AlertDialog.Builder builder, View view)
+  {
+    Resources r = builder.getContext ().getResources ();
+    int leftSpacing = r.getDimensionPixelSize (R.dimen.material_design_dialog_content_margin_left);
+    int rightSpacing = r.getDimensionPixelSize (R.dimen.material_design_dialog_content_margin_right);
+    int bottomSpacing = r.getDimensionPixelSize (R.dimen.material_design_dialog_content_margin_bottom);
+    int topSpacing = r.getDimensionPixelSize (R.dimen.material_design_dialog_content_margin_top);
+
+    builder.setView (view, leftSpacing, topSpacing, rightSpacing, bottomSpacing);
+  }
+}
