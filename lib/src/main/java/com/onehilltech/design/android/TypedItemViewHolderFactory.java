4b55d8ac0014d111285e7a3fbea4937a464a9914
James H. Hill <hilljh82@gmail.com>
2016-03-30 08:54:15 -0400

    Added support for dispatching activity results

--- lib/src/main/java/com/onehilltech/design/android/TypedItemViewHolderFactory.java
+++ lib/src/main/java/com/onehilltech/design/android/TypedItemViewHolderFactory.java
@@ -1,17 +1,22 @@
 package com.onehilltech.design.android;
 
+import android.support.annotation.NonNull;
 import android.view.View;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 
 public class TypedItemViewHolderFactory <T extends ItemAdapter.ViewHolder>
     extends SimpleItemViewHolderFactory
 {
   private final Class <T> clazz_;
 
-  public TypedItemViewHolderFactory (int layoutId, Class <T> clazz)
+  public TypedItemViewHolderFactory (int layoutId, @NonNull Class <T> clazz)
   {
     super (layoutId);
+
+    if ((clazz.getModifiers () & Modifier.STATIC) != 0)
+      throw new IllegalArgumentException ("Class cannot be static");
 
     this.clazz_ = clazz;
   }
