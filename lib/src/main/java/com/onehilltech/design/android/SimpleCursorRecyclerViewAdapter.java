68bdc3fed9bc3f7a5f0a2eec525b1b331d3936ff
James H. Hill <hilljh82@gmail.com>
2017-05-19 19:45:44 -0400

    SimpleCursorRecyclerViewAdapter added

---
+++ lib/src/main/java/com/onehilltech/design/android/SimpleCursorRecyclerViewAdapter.java
@@ -0,0 +1,66 @@
+package com.onehilltech.design.android;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.lang.reflect.Constructor;
+
+public class SimpleCursorRecyclerViewAdapter <VH extends SimpleCursorRecyclerViewAdapter.CursorViewHolder>
+    extends CursorRecyclerViewAdapter<VH>
+{
+  public static abstract class CursorViewHolder extends RecyclerView.ViewHolder
+  {
+    public CursorViewHolder (View itemView)
+    {
+      super (itemView);
+    }
+
+    public abstract void onBindData (Cursor cursor);
+  }
+
+  private final LayoutInflater inflater_;
+
+  private int layoutId_;
+
+  private Class<VH> viewHolderClass_;
+
+  public SimpleCursorRecyclerViewAdapter (Context context, Class<VH> viewHolderClass, int layoutId)
+  {
+    this (context, viewHolderClass, layoutId, null);
+  }
+
+  public SimpleCursorRecyclerViewAdapter (Context context, Class<VH> viewHolderClass, int layoutId, Cursor cursor)
+  {
+    super (context, cursor);
+
+    this.inflater_ = LayoutInflater.from (context);
+    this.viewHolderClass_ = viewHolderClass;
+    this.layoutId_ = layoutId;
+  }
+
+  @Override
+  public VH onCreateViewHolder (ViewGroup parent, int viewType)
+  {
+    View view = this.inflater_.inflate (this.layoutId_, parent, false);
+
+    try
+    {
+      Constructor<VH> ctor = this.viewHolderClass_.getConstructor (View.class);
+      return ctor.newInstance (view);
+    }
+    catch (Exception e)
+    {
+      throw new IllegalStateException (e.getLocalizedMessage (), e);
+    }
+  }
+
+  @Override
+  public void onBindViewHolder (VH viewHolder, Cursor cursor)
+  {
+    viewHolder.onBindData (cursor);
+  }
+}
